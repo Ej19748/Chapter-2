07d4ec8e0b3fbd81853e6d9c2720fb3740ea7343
Ej19748 <ej19748@email.vccs.edu>
2024-09-19 18:54:01 +0000

    .
    .

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() 
-{ 
- const float RISE_PER_YEAR = 1.5;
- float number_of_millimeters_each_year = 0;
- for (int i = 1; i <= 10; i++) 
-   {
-   number_of_millimeters_each_year += RISE_PER_YEAR;
- cout << "Year " << i << " : " << number_of_millimeters_each_year << endl;}
-return 0;}
+{
+return 0;
+}
