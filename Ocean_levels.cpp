f33854d5aecb841d08461cc271d842cb00f03e1d
Ej19748 <ej19748@email.vccs.edu>
2024-09-19 18:52:19 +0000

    Added text
    Added code and text

--- Ocean_levels.cpp
+++ Ocean_levels.cpp
@@ -1,3 +1,5 @@
+//Remember to use float for decimals
+//Remember 1.5 is constant
 #include <iostream>
 using namespace std;
 int main() 
@@ -9,3 +11,6 @@ int main()
    number_of_millimeters_each_year += RISE_PER_YEAR;
  cout << "Year " << i << " : " << number_of_millimeters_each_year << endl;}
 return 0;}
+//What I found most challenging was the for loop.
+//The problem I encoutered was that I forgot to add the semicolon after the for loop.
+//What I learned was how to use i++ to add 1 to i.
